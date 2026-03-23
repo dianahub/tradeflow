a4372ff472ddf61d5af7aedacf1d716bbde0612b
Diana Castillo <dianahelene@gmail.com>
2026-03-23 17:34:37 -0400

    fixed path

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\PositionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/positions', [PositionController::class, 'index']);
 });
